88cce22299dac8df09e28b026d4641de5e27b9ba
ArtemKomarec <artemkomarec10@gmail.com>
2022-02-10 14:26:17 +0300

    init: start edit-profile-Id-task feat: add edit component feat: add navigation to child component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {LoginComponent} from "./login/login.component";
-import {UsersComponent} from "./users/users.component";
-
+import { EditingComponent } from './editing/editing.component';
+import { LoginComponent } from "./login/login.component";
+import { UsersComponent } from "./users/users.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'profile', component: LoginComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: LoginComponent },
+  { path: 'users', 
+  children: [
+    { path: '', component: UsersComponent},
+    { path: ':id/edit', component: EditingComponent}
+  ] 
+},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
