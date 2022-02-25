b50b6e9c9b364087af6cdd94f48dd065454c18b8
ArtemKomarec <artemkomarec10@gmail.com>
2022-02-25 16:38:46 +0300

    refactor: delete unnecessary array refactor: change edit-dialog tests

--- src/app/editing/edit-dialog/edit-dialog.component.spec.ts
+++ src/app/editing/edit-dialog/edit-dialog.component.spec.ts
@@ -21,25 +21,25 @@ describe('EditDialogComponent', () => {
 
   it('set username', () => {
     const username = 'John';
-    component.name = 'John';
+    component.name = username;
     expect(component.name).toEqual(username);
   })
 
   it('set email', () => {
     const email = 'test@mail.com';
-    component.email = 'test@mail.com';
+    component.email = email;
     expect(component.email).toEqual(email);
   })
 
   it('set phone', () => {
     const phone = '375331111111';
-    component.phone = '375331111111';
+    component.phone = phone;
     expect(component.phone).toEqual(phone);
   })
 
   it('set site', () => {
     const site = 'www.test.com';
-    component.site = 'www.test.com';
+    component.site = site;
     expect(component.site).toEqual(site);
   })
 });
