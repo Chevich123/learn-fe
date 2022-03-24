b71a926a6b72fd1906330ffbb03d416be3766ac0
ArtemKomarec <artemkomarec10@gmail.com>
2022-03-24 12:24:04 +0300

    feat: add environment interface feat: add mock host api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
-export const environment = {
-  production: true
+import { IEnvironment } from './ienvironment';
+
+const HOST_API_URL = 'APPLICATION_API_URL';
+
+export const environment: IEnvironment = {
+  production: true,
+  HOST_API_URL
 };
