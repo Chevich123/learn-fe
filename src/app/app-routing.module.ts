67557da72244ffe48ca3c97a11cc548ad690389f
Bleckfi <pashkovskiy.serge@gmail.com>
2023-06-30 11:23:47 +0300

    end login form style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [{ path: 'login', component: LoginComponent }];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-fe';
+
+  show: boolean = true;
+  showLogin() {
+    this.show = !this.show;
+  }
 }
