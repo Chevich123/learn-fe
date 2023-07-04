800fba21718a3e6f7c7889e4e8bdb381be9131ce
Alexandr Yurkevich <jrkevichaa20@gmail.com>
2023-07-04 11:33:34 +0300

    added wrapper, changed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent }];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: WrapperComponent,
+    children: [
+      { path: 'home', component: DashboardComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
