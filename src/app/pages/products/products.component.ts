dbd8f184faf73705544f36b8243a5f299a9853cd
Alexandr Yurkevich <jrkevichaa20@gmail.com>
2023-07-13 16:16:02 +0300

    paginator finally bound to table

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,6 +3,7 @@ import {MatTableDataSource } from '@angular/material/table';
 import { ProductsService } from 'src/app/products.service';
 import { Product } from 'src/app/shared/interfaces/product';
 import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-products',
@@ -11,13 +12,17 @@ import { MatSort } from '@angular/material/sort';
 })
 
 export class ProductsComponent implements AfterViewInit,OnInit {
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
   @ViewChild(MatSort)
-  sort = new MatSort()
+  sort!: MatSort;
+
   displayedColumns: string[] = ['country_of_origin', 'manufacturer', 'name', 'width', 'height', 'depth'];
   dataSource = new MatTableDataSource<Product>([]);
   constructor(private productsService: ProductsService){ }
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
   ngOnInit(): void {
     this.productsService.getProducts().subscribe({
