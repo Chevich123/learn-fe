2aefc81f4c1339126900623fdba54964d51b5ca2
Dima Rogachevich <dima.rogachevich@gmail.com>
2023-07-19 19:50:49 +0300

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,4 @@ import { filter, map } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private router: Router, private titleService: Title) {}
-
-  ngOnInit() {
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        map(() => {
-          let route: ActivatedRoute = this.router.routerState.root;
-          let routeTitle = '';
-          while (route!.firstChild) {
-            route = route.firstChild;
-          }
-          if (route.snapshot.data['title']) {
-            routeTitle = route!.snapshot.data['title'];
-          }
-          return routeTitle;
-        }),
-      )
-      .subscribe((title: string) => {
-        if (title) {
-          this.titleService.setTitle(`My App - ${title}`);
-        }
-      });
-  }
-}
+export class AppComponent {}
