2005e671b4b0e0d938c4623e582ed38db45e673f
Dima Rogachevich <dima.rogachevich@gmail.com>
2023-07-19 14:49:03 +0300

    Title added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,17 +13,48 @@ const routes: Routes = [
     path: '',
     component: WrapperComponent,
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'products', children : [
-        { path: 'add', component: AddProductComponent },
-        { path: '', component: ProductsComponent, pathMatch: 'full'}
-      ]},
-      { path: 'home', component: DashboardComponent },
+      {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login | Workshop #3',
+      },
+
+      {
+        path: 'products',
+        children: [
+          {
+            path: 'add',
+            component: AddProductComponent,
+            title: 'New product | Workshop #3',
+          },
+          {
+            path: '',
+            component: ProductsComponent,
+            pathMatch: 'full',
+            title: 'Products | Workshop #3',
+          },
+        ],
+      },
+
+      {
+        path: 'home',
+        component: DashboardComponent,
+        title: 'Home | Workshop #3',
+      },
+
       {
         path: 'users',
         children: [
-          { path: '', component: UsersComponent },
-          { path: 'add', component: AddUserComponent },
+          {
+            path: '',
+            component: UsersComponent,
+            title: 'Users | Workshop #3',
+          },
+          {
+            path: 'add',
+            component: AddUserComponent,
+            title: 'New user | Workshop #3',
+          },
         ],
       },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
