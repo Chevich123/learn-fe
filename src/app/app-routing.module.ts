29e1b87b06397c00347c1f229c204697b16ebe82
DmitryChevich <137980209+DmitryChevich@users.noreply.github.com>
2023-07-20 12:38:06 +0300

    Title added (#13)
    * Title added
    
    * now with titleStratagy
    
    * Workshop #3 is a link
    
    * fixed
    
    * import fixed
    
    * "untitle" title added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import {
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+  TitleStrategy,
+} from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { AddProductComponent } from './pages/products/add/add.component';
@@ -7,23 +12,71 @@ import { WrapperComponent } from './wrapper/wrapper.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { UsersComponent } from './pages/users/users.component';
 import { AddUserComponent } from './pages/users/add-user/add-user.component';
+import { Title } from '@angular/platform-browser';
+
+@Injectable()
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`${title} | Workshop #3`);
+    } else {
+      this.title.setTitle('Workshop #3');
+    }
+  }
+}
 
 const routes: Routes = [
   {
     path: '',
     component: WrapperComponent,
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'products', children : [
-        { path: 'add', component: AddProductComponent },
-        { path: '', component: ProductsComponent, pathMatch: 'full'}
-      ]},
-      { path: 'home', component: DashboardComponent },
+      {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login',
+      },
+
+      {
+        path: 'products',
+        children: [
+          {
+            path: 'add',
+            component: AddProductComponent,
+            title: 'New product',
+          },
+          {
+            path: '',
+            component: ProductsComponent,
+            pathMatch: 'full',
+            title: 'Products',
+          },
+        ],
+      },
+
+      {
+        path: 'home',
+        component: DashboardComponent,
+        title: 'Home',
+      },
+
       {
         path: 'users',
         children: [
-          { path: '', component: UsersComponent },
-          { path: 'add', component: AddUserComponent },
+          {
+            path: '',
+            component: UsersComponent,
+            title: 'Users',
+          },
+          {
+            path: 'add',
+            component: AddUserComponent,
+            title: 'New user',
+          },
         ],
       },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -34,5 +87,11 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [
+    {
+      provide: TitleStrategy,
+      useClass: TemplatePageTitleStrategy,
+    },
+  ],
 })
 export class AppRoutingModule {}
