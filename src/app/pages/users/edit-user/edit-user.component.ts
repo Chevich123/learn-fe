cc8691af740be861a3c1431bb17f5876ded3dca9
Bleckfi <102161222+Bleckfi@users.noreply.github.com>
2023-08-02 20:06:37 +0300

    photo in users (#25)
    * photo in users
    
    * clear
    
    * fix

--- src/app/pages/users/edit-user/edit-user.component.ts
+++ src/app/pages/users/edit-user/edit-user.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormControl,
-  FormBuilder,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IUser } from 'src/app/shared/interfaces/user';
 import { AddUserComponent } from '../add-user/add-user.component';
 import { UserService } from '../../../services/user.service';
+import { ImageService } from '../../../services/image.service';
+import { of, switchMap } from 'rxjs';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-edit-user',
@@ -21,21 +19,38 @@ export class EditUserComponent implements OnInit {
     private usersService: UserService,
     private router: Router,
     private route: ActivatedRoute,
-  ) {}
-
+    private imageService: ImageService,
+  ) {
+    this.userId = route.snapshot.params['id'];
+    this.usersService
+      .getUser(this.userId!)
+      .pipe(
+        switchMap((user) => {
+          this.userForm.patchValue(user);
+          return user.avatar
+            ? this.imageService.imagePreview(user.avatar)
+            : of(undefined);
+        }),
+      )
+      .subscribe((safeUrl) => {
+        this.preview = safeUrl;
+      });
+  }
+  preview: SafeUrl | undefined;
   userId!: string;
   initialUser!: IUser;
   userForm: FormGroup = this.formBuilder.group({
     username: ['', [Validators.required, Validators.minLength(4)]],
     email: ['', Validators.email],
     site: [''],
+    avatar: [''],
     phone: ['', AddUserComponent.phoneVal],
   });
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.params['id'];
-    this.loadUser(this.userId);
   }
+
   loadUser(userId: string) {
     this.usersService.getUser(userId).subscribe({
       next: (result) => {
@@ -52,5 +67,33 @@ export class EditUserComponent implements OnInit {
         next: () => this.router.navigateByUrl('/users'),
         error: (err) => console.log(err),
       });
+  }
+
+  onDrop(event: DragEvent) {
+    if (!event.dataTransfer?.files[0].type.includes('image/')) return;
+    event.preventDefault();
+    this.uploadImage(event.dataTransfer?.files[0]);
+  }
+
+  uploadImage(file: File | undefined) {
+    if (!file) return;
+    const formdata = new FormData();
+    formdata.append('file', file);
+    this.imageService
+      .uploadImage(formdata)
+      .pipe(
+        switchMap((result: { originalname: string; filename: string }) => {
+          this.userForm.patchValue({ avatar: result.filename });
+          return this.imageService.imagePreview(result.filename);
+        }),
+      )
+      .subscribe((safeUrl) => {
+        this.preview = safeUrl;
+        this.userForm.markAsDirty();
+      });
+  }
+
+  onFileSelected(event: any) {
+    this.uploadImage(event.target.files[0]);
   }
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from '../../shared/interfaces/user';
 import { UserService } from '../../services/user.service';
-import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDeleteComponent } from '../products/confirm-delete/confirm-delete.component';
-
+import { map, mergeAll, mergeMap, of, toArray } from 'rxjs';
+import { ImageService } from '../../services/image.service';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-  constructor(private userService: UserService, private dialog: MatDialog) {}
+  constructor(
+    private userService: UserService,
+    private dialog: MatDialog,
+    private imageService: ImageService,
+  ) {}
 
   columns = [
     {
@@ -40,18 +45,33 @@ export class UsersComponent implements OnInit {
       cell: (user: IUser) => user.site,
     },
   ];
-  dataSource: IUser[] = [];
+  dataSource = new MatTableDataSource<IUser>([]);
   displayedColumns = [
     ...this.columns.map((c) => c.columnDef),
+    'image',
     'edit',
     'delete',
   ];
 
-  getUsers() {
-    this.userService.getUsers().subscribe({
-      next: (usersResponse) => (this.dataSource = usersResponse.data),
-      error: (error) => console.error('Error fetching users:', error),
-    });
+  private getUsers() {
+    this.userService
+      .getUsers()
+      .pipe(
+        map((data) => data.data),
+        mergeAll(),
+        mergeMap((user: IUser) => {
+          if (user.avatar) {
+            return this.imageService
+              .imagePreview(user.avatar)
+              .pipe(map((safeUrl) => ({ ...user, imagePreview: safeUrl })));
+          }
+          return of(user);
+        }),
+        toArray(),
+      )
+      .subscribe((array) => {
+        this.dataSource.data = array;
+      });
   }
 
   deleteUser(userID: string): void {
